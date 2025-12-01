1e551be74df10c320058a4e8b5b1b0149e44a6de
csbking <alexhuntercsb@gmail.com>
2025-12-01 10:32:58 +0600

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name="AutoCardChecker",
-    ext_modules=cythonize("card_checker.pyx", compiler_directives={'language_level' : "3"})
-)
